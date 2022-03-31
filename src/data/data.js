ffaf386e29b624b8c74d4fb57a779be0e4344e8c
Redoqx <ridho.pmswa@gmail.com>
2022-03-31 08:05:23 +0700

    First

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+const LoginInfo = [
+    {
+        loginId: 1,
+        userName: 'Admin1',
+        password: 'Plm123?><',
+        role: 'Admin',
+    },
+    {
+        loginId: 2,
+        userName: 'Karyawan1',
+        password: '!@#qwe456',
+        role: 'Karyawan',
+    }
+];
